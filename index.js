4f26b49ad66746b0b9106625a705a597c9540291
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-11-08 15:25:36 +0900

    get engrave, classIMG, ItemLevel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const playwright = require('playwright');
 const jsdom = require("jsdom");
+
 async function getData(charactername){
   for (const browserType of [playwright.chromium]) {
     const browser = await browserType.launch();
@@ -8,14 +9,39 @@ async function getData(charactername){
     const page = await context.newPage();
     await page.goto(`https://lostark.game.onstove.com/Profile/Character/${charactername}`)
     
-    
-
     const content = await page.content();
     //console.log(content);
     await browser.close();
     
     const dom = new jsdom.JSDOM(content);
+
+     // Click [aria-label="Next slide"]
+    //await page.click('[aria-label="Next slide"]');
     
+    engravearr=[]
+    
+    dom.window.document.querySelectorAll("ul[class^='swiper-slide'] > li > span").forEach(element => {
+        engravearr.push(element.textContent)
+    });
+
+    console.log("=======================\n")
+    console.log(engravearr)
+
+
+    
+
+    // 직업 로고
+    /*
+    temp = dom.window.document.querySelector("#lostark-wrapper > div > main > div > div.profile-character-info > img")
+    temp=temp.src
+    console.log(temp)
+    */
+    // 아이템 레벨
+    /*
+    temp =getdata_bypath(dom,"#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__expedition > span:nth-child(2)")
+    console.log(temp) */
+    // 전투특성
+    /* 
     combat_Traits =[]
     combat_Traits_names=["치명","특화","제압","신속","인내","숙련"]
     for (let index = 0; index < 5; index++) {
@@ -25,8 +51,13 @@ async function getData(charactername){
     for (let index = 0 ; index<5; index++){
         console.log(`${combat_Traits_names[index]} : ${combat_Traits[index]}`)
     }
-    
+    */
+
   }
 };
 
-getData("슈사이어만키우는사람")
+function getdata_bypath(dom,path){
+    return dom.window.document.querySelector(path).textContent
+}
+
+getData("용카리나")
