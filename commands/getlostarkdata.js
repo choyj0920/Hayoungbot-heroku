a42d33e21a77efbba9baf1e7af748dd83d6e879e
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-11-09 14:47:18 +0900

    Update getlostarkdata.js

--- commands/getlostarkdata.js
+++ commands/getlostarkdata.js
@@ -1,60 +1,61 @@
 
-const playwright = require('playwright');
+const { chromium } = require("playwright-chromium");
+
 const jsdom = require("jsdom");
 const Discord = require('discord.js')
 
 async function getData(charactername){
-  for (const browserType of [playwright.chromium]) {
-    const browser = await browserType.launch({ chromiumSandbox: false });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto(`https://lostark.game.onstove.com/Profile/Character/${charactername}`)
-    
-    const content = await page.content();
-    //console.log(content);
-    
-    const dom = new jsdom.JSDOM(content);
-    
-     // Click [aria-label="Next slide"]
-    //await page.click('[aria-label="Next slide"]');
-    
-    characterData={}
-    characterData["Character_Name"]=charactername
+  
+  const browser = await chromium.launch({ chromiumSandbox: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(`https://lostark.game.onstove.com/Profile/Character/${charactername}`)
+  
+  const content = await page.content();
+  //console.log(content);
+  
+  const dom = new jsdom.JSDOM(content);
+  
+    // Click [aria-label="Next slide"]
+  //await page.click('[aria-label="Next slide"]');
+  
+  characterData={}
+  characterData["Character_Name"]=charactername
 
-    engravearr=[]
-    
-    dom.window.document.querySelectorAll("ul[class^='swiper-slide'] > li > span").forEach(element => {
-        engravearr.push(element.textContent)
-    });
-    characterData["Engrave"]=engravearr
-    
+  engravearr=[]
+  
+  dom.window.document.querySelectorAll("ul[class^='swiper-slide'] > li > span").forEach(element => {
+      engravearr.push(element.textContent)
+  });
+  characterData["Engrave"]=engravearr
+  
 
-    // 직업 로고
-    
-    temp = dom.window.document.querySelector("#lostark-wrapper > div > main > div > div.profile-character-info > img")
-    classlogo=temp.src
-    
-    characterData['Class_Logo']=classlogo
-    
-    // 아이템 레벨
-    
-    level =getdata_bypath(dom,"#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__expedition > span:nth-child(2)")
-    characterData['Level']=level
-    // 전투특성
-    
-    combat_Traits ={}
-    combat_Traits_names=["치명","특화","제압","신속","인내","숙련"]
-    for (let index = 0; index < 6; index++) {
-        temp= dom.window.document.querySelector(`#profile-ability > div.profile-ability-battle > ul > li:nth-child(${index+1}) > span:nth-child(2)`).textContent
-        combat_Traits[combat_Traits_names[index]]=temp
-    }
-    
-    characterData["Combat_Traits"]=combat_Traits
-
-    await browser.close();
-
-    return characterData
+  // 직업 로고
+  
+  temp = dom.window.document.querySelector("#lostark-wrapper > div > main > div > div.profile-character-info > img")
+  classlogo=temp.src
+  
+  characterData['Class_Logo']=classlogo
+  
+  // 아이템 레벨
+  
+  level =getdata_bypath(dom,"#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__expedition > span:nth-child(2)")
+  characterData['Level']=level
+  // 전투특성
+  
+  combat_Traits ={}
+  combat_Traits_names=["치명","특화","제압","신속","인내","숙련"]
+  for (let index = 0; index < 6; index++) {
+      temp= dom.window.document.querySelector(`#profile-ability > div.profile-ability-battle > ul > li:nth-child(${index+1}) > span:nth-child(2)`).textContent
+      combat_Traits[combat_Traits_names[index]]=temp
   }
+  
+  characterData["Combat_Traits"]=combat_Traits
+
+  await browser.close();
+
+  return characterData
+
 };
 
 module.exports = {
